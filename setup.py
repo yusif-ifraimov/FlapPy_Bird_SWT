7869a97d271fce064ce652c2397b2d2795e34fa6
yusif-ifraimov <56882012+yusif-ifraimov@users.noreply.github.com>
2020-02-14 03:07:45 +0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="FlapPy_Bird",
+    version='1.0',
+    packages=['src'],
+    url='https://github.com/yusif-ifraimov/FlapPy_Bird_SWT',
+    license='MIT',
+    author='YusifIfraimov',
+    author_email='ifraimovyusif@gmail.com',
+    description='A Python version of famous Flappy Bird Game.'
+)
